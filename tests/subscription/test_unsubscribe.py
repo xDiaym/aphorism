e8d750c5210f0f7d1496e2c85db0a8dfc192947b
Matthew Nekirov <matthew.nekirov@gmail.com>
2022-04-07 00:48:41 +0300

    test(subscription): add test for unsubscribe method

---
+++ tests/subscription/test_unsubscribe.py
@@ -0,0 +1,58 @@
+from http import HTTPStatus
+from typing import Callable
+
+from flask import Flask
+from flask.testing import FlaskClient
+from werkzeug.test import TestResponse
+
+from aphorism.apps.subscription.model import subscriptions
+from aphorism.apps.user.model import User
+from tests.subscription import subscribe
+from tests.conftest import RegisteredUser
+
+
+def unsubscribe(
+    client: FlaskClient,
+    token: None | str,
+    publisher: str,
+) -> TestResponse:
+    headers = {"Authorization": f"Bearer {token}"} if token else {}
+    return client.delete(
+        f"/api/v1/subscription/{publisher}",
+        headers=headers,
+    )
+
+
+def test_unsubscribe_on_user_that_does_not_exist(
+    client: FlaskClient,
+    registered_user: RegisteredUser,
+) -> None:
+    response = unsubscribe(client, registered_user.token, "does-not-exists")
+    assert response.status_code == int(HTTPStatus.NOT_FOUND)
+
+
+def test_subscription_without_token(
+    client: FlaskClient,
+    registered_user: RegisteredUser,
+) -> None:
+    response = unsubscribe(client, None, registered_user.slug)
+    assert response.status_code == int(HTTPStatus.UNAUTHORIZED)
+
+
+def test_unsubscribe_ok(
+    app: Flask,
+    client: FlaskClient,
+    registered_user_fabric: Callable[[], RegisteredUser],
+) -> None:
+    u1, u2 = registered_user_fabric(), registered_user_fabric()
+    subscribe(client, u2.token, u1.slug)
+
+    response = unsubscribe(client, u2.token, u1.slug)
+    assert response.data
+    assert response.status_code == int(HTTPStatus.OK)
+
+    with app.app_context():
+        u = User.query.join(subscriptions, subscriptions.c.subscriber_id == User.id).first()
+        assert u is None
+
+# TODO: add more tests
